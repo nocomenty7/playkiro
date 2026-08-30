2330fa79d8ac931ba24e37254354dd26bfd0c75c
nocomenty7 <nocomenty7@gmail.com>
2026-08-30 21:20:09 +0900

    fix: update domain from .kr to .com in sitemap and robots for SEO

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://playkiro.kr/sitemap.xml',
+    sitemap: 'https://playkiro.com/sitemap.xml',
   };
 }
